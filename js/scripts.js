0fc6c64c025f885e739123a220237811377ecf0b
maina murage <mainamurage21@gmail.com>
2019-02-19 09:05:43 +0300

    set function to retririeve information that has been input

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+//Business logic
+var track = function(first, second, third){
+  var firstlength = document.getElementById("firstl").value;
+  var secondlength = document.getElementById("secondl").value;
+  var thirdlength = document.getElementById("thirdl").value;
